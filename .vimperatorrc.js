8ecd6ef9eb0ff105a091ad8c0f704611a7a96b4e
janus_wel <janus.wel.3@gmail.com>
2011-04-24 20:38:14 +0900

    Move js codes to another file

--- .vimperatorrc.js
+++ .vimperatorrc.js
@@ -1,6 +1,39 @@
 // .vimperator.js
 // janus_wel <janus.wel.3@gmail.com>
 
+// options {{{1
+// don't start new vim {{{2
+if (liberator.has('MacUnix')) {
+    liberator.modules.options.editor='/Applications/MacVim.app/Contents/MacOS/MacVim -f --remote-tab-wait-silent';
+}
+else if (liberator.has('Win32')) {
+    liberator.modules.options.editor='gvim -f --remote-tab-wait-silent';
+}
+
+
+// mappings {{{1
+// commands like win32 {{{2
+let modifier = 'C';
+let redo_key = '<C-y>';
+if (liberator.has('MacUnix')) {
+    modifier = 'M';
+    redo_key = '<S-M-z>';
+}
+
+liberator.execute('noremap <C-a> <C-v><' + modifier + '-a>', null, true);
+liberator.execute('noremap <C-c> <C-v><' + modifier + '-c>', null, true);
+liberator.execute('noremap <C-s> <C-v><' + modifier + '-s>', null, true);
+
+liberator.execute('inoremap <C-x> <C-v><' + modifier + '-x>', null, true);
+liberator.execute('inoremap <C-v> <C-v><' + modifier + '-v>', null, true);
+liberator.execute('inoremap <C-z> <C-v><' + modifier + '-z>', null, true);
+liberator.execute('inoremap <C-y> <C-v>' + redo_key, null, true);
+
+liberator.execute('cnoremap <C-x> <C-v><' + modifier + '-x>', null, true);
+liberator.execute('cnoremap <C-v> <C-v><' + modifier + '-v>', null, true);
+liberator.execute('cnoremap <C-z> <C-v><' + modifier + '-z>', null, true);
+liberator.execute('cnoremap <C-y> <C-v>' + redo_key, null, true);
+
 
 // finish {{{1
 liberator.echo('.vimperator.js sourced.');
