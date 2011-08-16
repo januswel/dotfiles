66fbb4bd19a696a0bdf8c6c28e0843937f8dfb4f
janus_wel <janus.wel.3@gmail.com>
2011-08-16 20:55:34 +0900

    Fix Windows-like mappings in input & command mode
    Remove unnecessary key "<C-v>" with,
    
        * <C-a>, <C-c>, <C-x>, <C-v>, <C-z>, <C-y> in input mode
        * <C-c>, <C-x>, <C-v>, <C-z>, <C-y> in command mode

--- .vimperator/mappings.js
+++ .vimperator/mappings.js
@@ -22,17 +22,17 @@ liberator.execute('noremap <C-7> <C-v><' + modifier + '-7>', null, true);
 liberator.execute('noremap <C-8> <C-v><' + modifier + '-8>', null, true);
 liberator.execute('noremap <C-9> <C-v><' + modifier + '-9>', null, true);
 
-liberator.execute('inoremap <C-a> <C-v><' + modifier + '-a>', null, true);
-liberator.execute('inoremap <C-c> <C-v><' + modifier + '-c>', null, true);
-liberator.execute('inoremap <C-x> <C-v><' + modifier + '-x>', null, true);
-liberator.execute('inoremap <C-v> <C-v><' + modifier + '-v>', null, true);
-liberator.execute('inoremap <C-z> <C-v><' + modifier + '-z>', null, true);
-liberator.execute('inoremap <C-y> <C-v>' + redo_key, null, true);
+liberator.execute('inoremap <C-a> <' + modifier + '-a>', null, true);
+liberator.execute('inoremap <C-c> <' + modifier + '-c>', null, true);
+liberator.execute('inoremap <C-x> <' + modifier + '-x>', null, true);
+liberator.execute('inoremap <C-v> <' + modifier + '-v>', null, true);
+liberator.execute('inoremap <C-z> <' + modifier + '-z>', null, true);
+liberator.execute('inoremap <C-y> ' + redo_key, null, true);
 
-liberator.execute('cnoremap <C-c> <C-v><' + modifier + '-c>', null, true);
-liberator.execute('cnoremap <C-x> <C-v><' + modifier + '-x>', null, true);
-liberator.execute('cnoremap <C-v> <C-v><' + modifier + '-v>', null, true);
-liberator.execute('cnoremap <C-z> <C-v><' + modifier + '-z>', null, true);
-liberator.execute('cnoremap <C-y> <C-v>' + redo_key, null, true);
+liberator.execute('cnoremap <C-c> <' + modifier + '-c>', null, true);
+liberator.execute('cnoremap <C-x> <' + modifier + '-x>', null, true);
+liberator.execute('cnoremap <C-v> <' + modifier + '-v>', null, true);
+liberator.execute('cnoremap <C-z> <' + modifier + '-z>', null, true);
+liberator.execute('cnoremap <C-y> ' + redo_key, null, true);
 
 // vim: ts=4 sw=4 sts=4 et fdm=marker fdc=3
