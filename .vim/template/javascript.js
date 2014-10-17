434c1882acedf9183ccd91af98e6b27b0117c8fb
janus_wel <janus.wel.3@gmail.com>
2014-10-18 00:12:07 +0900

    Update library version

--- .vim/template/javascript.js
+++ .vim/template/javascript.js
@@ -4,11 +4,11 @@
 var require = {
     baseUrl: './scripts',
     paths: {
-        jquery: 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
-        'jQuery.UI': 'http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min',
-        Backbone: 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
-        underscore: 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min',
-        json2: 'http://cdnjs.cloudflare.com/ajax/libs/json2/20121008/json2'
+        jquery: 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
+        'jQuery.UI': 'http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
+        Backbone: 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+        underscore: 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
+        json2: 'http://cdnjs.cloudflare.com/ajax/libs/json2/20130526/json2.min'
     },
     shim: {
         'jQuery.UI': ['jquery'],
