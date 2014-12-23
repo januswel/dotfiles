82ff7aac4a4de1b7f3f1c272dc5c88010b1231b1
janus_wel <janus.wel.3@gmail.com>
2014-12-24 01:15:01 +0900

    Remove unused library settings

--- .vim/template/javascript.js
+++ .vim/template/javascript.js
@@ -4,18 +4,12 @@ var require = {
     paths: {
         jquery: 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
         'jQuery.UI': 'http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
-        Backbone: 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
-        underscore: 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
-        json2: 'http://cdnjs.cloudflare.com/ajax/libs/json2/20130526/json2.min'
+        underscore: 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min'
     },
     shim: {
         'jQuery.UI': ['jquery'],
         underscore: {
             exports: '_'
-        },
-        Backbone: {
-            deps: ['underscore', 'json2', 'jquery'],
-            exports: 'Backbone'
         }
     }
 };
