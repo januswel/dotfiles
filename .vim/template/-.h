05514231d50903c58da381f65115ea277b1e2c34
janus_wel <janus.wel.3@gmail.com>
2015-11-15 04:09:46 +0900

    add template for C language header

---
+++ .vim/template/-.h
@@ -0,0 +1,13 @@
+/*
+* "%:t"
+*  remarks
+*
+*  written by janus_wel<janus.wel.3@gmail.com>
+*  This source code is in public domain, and has NO WARRANTY.
+* */
+
+#ifndef substitute(substitute(expand('%:t'),'\.','_',''),'.*','\U\0','')
+#define substitute(substitute(expand('%:t'),'\.','_',''),'.*','\U\0','')
+
+#endif  // substitute(substitute(expand('%:t'),'\.','_',''),'.*','\U\0','')
+
