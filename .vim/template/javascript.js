0909e094593a2ab0bfdb31ef7f6a7c8d91715ca5
janus_wel <janus.wel.3@gmail.com>
2016-05-03 14:38:31 +0900

    add codes for coding contest

--- .vim/template/javascript.js
+++ .vim/template/javascript.js
@@ -1,4 +1,19 @@
 'use strict';
 
+var solver = function (input) {
+    var answer = input;
+    return answer;
+};
 
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
+var input = '';
+process.stdin.on('data', function (chunk) {
+    input += chunk;
+});
+process.stdin.on('end', function () {
+    var lines = input.trim().split('\n');
+    var answer = solver(lines);
+    console.log(answer);
+});
