4dc09f52aa0ad503d627e80e98c55032e230e856
Takagi Kensuke <janus.wel.3@gmail.com>
2019-01-28 01:01:36 +0900

    :wrench: update snippet for sh scripts

--- .vim/template/sh.sh
+++ .vim/template/sh.sh
@@ -2,51 +2,11 @@
 
 set -eux
 
-# default settings
-BAR="buz"
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+echo "${SCRIPT_DIR}"
 
-# environments
-COMMAND_NAME=$(basename $0)
+ROOT=$(cd "$(dirname "$0")/.."; pwd)
+echo "${ROOT}"
 
-# functions
-function get_script_dir() {
-    echo "$(dirname "${BASH_SOURCE:-$0}")"
-    return 0
-}
-
-function absolute_dir() {
-    if [ $# -ne 1 ]; then
-        echo 'Usage: absolute_dir DIR' 1>&2
-        return 1
-    fi
-
-    echo "$(cd "$1"; pwd)"
-    return 0
-}
-
-function absolute_leaf() {
-    if [ $# -ne 1 ]; then
-        echo 'Usage: absolute_dir FILE' 1>&2
-        return 1
-    fi
-
-    echo ""$(absolute_dir "$(dirname "$1")")"/"$(basename "$1")""
-    return 0
-}
-
-# options
-while getopts fb: OPT
-do
-    case ${OPT} in
-        "f" ) FLAG=1 ;;
-        "b" ) BAR=${OPTARG} ;;
-          * ) echo "Usage: ${COMMAND_NAME} [-f] [-b <arg>] <input>"
-              exit 2 ;;
-    esac
-done
-
-shift $(expr $OPTIND - 1)
-
-# execute
-ABSOLUTE_PATH=$(absolute_path "$1")
-echo $ABSOLUTE_PATH
+COMMAND_NAME=$(basename "$0")
+echo "${COMMAND_NAME}"
