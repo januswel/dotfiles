ef3a07d28e2c517592294cc660c6118f3817440c
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-29 17:45:15 +0900

    :art: fix lint warnings

--- bin/ss2.sh
+++ bin/ss2.sh
@@ -2,15 +2,15 @@
 
 # change location to save screenshots in Darwin
 
-if [ "Darwin" != $(uname) ]; then
+if [ "Darwin" != "$(uname)" ]; then
     exit 1
 fi
 
 PLIST="com.apple.screencapture"
 KEY="location"
 
-if [ $1 != "" ]; then
-    defaults write ${PLIST} ${KEY} $1
+if [ "$1" != "" ]; then
+    defaults write ${PLIST} ${KEY} "$1"
 else
     defaults delete ${PLIST} ${KEY}
 fi
