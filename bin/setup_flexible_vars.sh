eb21f530153353a737a27eb38e2af89fc90ce0b5
Takagi Kensuke <janus.wel.3@gmail.com>
2019-10-29 10:45:27 +0900

    :wrench: update path settings for Android Studio

--- bin/setup_flexible_vars.sh
+++ bin/setup_flexible_vars.sh
@@ -42,7 +42,7 @@ if [ "Darwin" = "$(uname)" ]; then
 
     # for Android development
     export ANDROID_HOME=${HOME}/Library/Android/sdk
-    export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+    export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
 
     # for Flutter
     export FLUTTER_HOME=${HOME}/lib/flutter
