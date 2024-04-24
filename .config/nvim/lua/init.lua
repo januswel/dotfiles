935aec2b89ca28c72a2373f2e97d7a71829e81d9
Takagi Kensuke <janus.wel.3@gmail.com>
2024-04-24 15:50:34 +0900

    :bug: fix init.lua position

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -3,16 +3,30 @@ if vim.g.vscode then
   local function moveCursor(to, select)
     return function()
       vscode.action('cursorMove', {
-      args = {
-        {
-          to = to,
-          by = 'wrappedLine',
-          value = vim.v.count1,
-          select = select
+        args = {
+          {
+            to = to,
+            by = 'wrappedLine',
+            value = vim.v.count1,
+            select = select
+          },
         },
-      },
-    })
-    return '<Ignore>'
+      })
+      return '<Ignore>'
+    end
+  end
+
+  local function moveCursorInVisualMode(to)
+    return function()
+      local mode = vim.api.nvim_get_mode()
+      if mode.mode == 'V' or mode.mode == 'CTRL-V' then
+        if to == 'up' then
+          vscode.action('cursorColumnSelectUp')
+        else if to == 'down' then
+          vscode.action('cursorColumnSelectDown')
+        end
+      end
+      return '<Ignore>'
     end
   end
 
@@ -22,8 +36,8 @@ if vim.g.vscode then
   vim.keymap.set('n', '^', moveCursor('wrappedLineFirstNonWhitespaceCharacter'), { expr = true })
   vim.keymap.set('n', '$', moveCursor('wrappedLineEnd'), { expr = true })
 
-  vim.keymap.set('v', 'k', moveCursor('up', true), { expr = true })
-  vim.keymap.set('v', 'j', moveCursor('down', true), { expr = true })
+  vim.keymap.set('v', 'k', moveCursorInVisualMode('up', true), { expr = true })
+  vim.keymap.set('v', 'j', moveCursorInVisualMode('down', true), { expr = true })
   vim.keymap.set('v', '0', moveCursor('wrappedLineStart', true), { expr = true })
   vim.keymap.set('v', '^', moveCursor('wrappedLineFirstNonWhitespaceCharacter', true), { expr = true })
   vim.keymap.set('v', '$', moveCursor('wrappedLineEnd', true), { expr = true })
