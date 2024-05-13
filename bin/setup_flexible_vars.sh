c36f4e0af690f14b76b0d19af72cbee6b48591af
Takagi Kensuke <janus.wel.3@gmail.com>
2024-05-14 02:29:14 +0900

    🚨 fix linter warnings

--- bin/setup_flexible_vars.sh
+++ bin/setup_flexible_vars.sh
@@ -1,72 +1,72 @@
+#!/bin/sh
+
 # shellcheck disable=SC2139
 # shellcheck disable=SC1090
 
 # environment variables
-export CPLUS_INCLUDE_PATH=/usr/local/include:$CPLUS_INCLUDE_PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export CPLUS_INCLUDE_PATH=/usr/local/include:"${CPLUS_INCLUDE_PATH}"
+export LD_LIBRARY_PATH=/usr/local/lib:"${LD_LIBRARY_PATH}"
 
 # settings for macOS
 if [ "Darwin" = "$(uname)" ]; then
     # for HomeBrew
-    export PATH=${PATH}:/usr/local/sbin
-    export MANPATH=${MANPATH}:/usr/local/share/man
-    export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:/usr/local/include
+    export PATH="${PATH}":/usr/local/sbin
+    export MANPATH="${MANPATH}":/usr/local/share/man
+    export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH}":/usr/local/include
 
     # coreutils
     COREUTILS_PATH=$(brew --prefix coreutils)
-    export PATH=${COREUTILS_PATH}/libexec/gnubin:${PATH}
+    export PATH="${COREUTILS_PATH}/libexec/gnubin":"${PATH}"
 
     # for anyenv
     eval "$(anyenv init - zsh)"
 
     # for git
     GIT_PATH=$(brew --prefix git)
-    export PATH=${PATH}:${GIT_PATH}/bin
+    export PATH="${GIT_PATH}/bin":"${PATH}"
 
     # for Java
     JAVA_HOME=$(/usr/libexec/java_home -v 21)
     export JAVA_HOME
-    export PATH=${PATH}:${JAVA_HOME}/bin
+    export PATH="${JAVA_HOME}/bin":"${PATH}"
     alias java="java -Dfile.encoding=UTF-8"
     alias javac="javac -J-Dfile.encoding=UTF-8"
 
     # for Rust
     if [ -d "${HOME}/.cargo/bin" ]; then
-      export PATH=${PATH}:${HOME}/.cargo/bin
+      export PATH="${HOME}/.cargo/bin":"${PATH}"
     fi
 
     # for Android development
-    export ANDROID_HOME=${HOME}/Library/Android/sdk
+    export ANDROID_HOME="${HOME}/Library/Android/sdk"
     ANDROID_EMULATOR=${ANDROID_HOME}/emulator
     ANDROID_TOOLS=${ANDROID_HOME}/tools
     ANDROID_BIN=${ANDROID_HOME}/tools/bin
     ANDROID_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools
-    export PATH=${PATH}:${ANDROID_EMULATOR}:${ANDROID_TOOLS}:${ANDROID_BIN}:${ANDROID_PLATFORM_TOOLS}
-
-    # for Flutter
-    export FLUTTER_HOME=${HOME}/lib/flutter
-    export PATH=${PATH}:${FLUTTER_HOME}/bin
+    export PATH="${ANDROID_EMULATOR}":"${ANDROID_TOOLS}":"${ANDROID_BIN}":"${ANDROID_PLATFORM_TOOLS}":"${PATH}"
 
     # for google-cloud-sdk
     if [ -d "${HOME}/google-cloud-sdk/bin" ]; then
-      export PATH=${PATH}:${HOME}/google-cloud-sdk/bin
+      export PATH="${HOME}/google-cloud-sdk/bin":"${PATH}"
     fi
 
     # MySQL
     MYSQL_PATH=$(brew --prefix mysql-client)
-    export PATH=${PATH}:${MYSQL_PATH}/bin
+    export PATH="${MYSQL_PATH}/bin":"${PATH}"
 
     # PostgreSQL
     LIBPQ_PATH=$(brew --prefix libpq)
-    export PATH=${PATH}:${LIBPQ_PATH}/bin
+    export PATH="${LIBPQ_PATH}/bin":"${PATH}"
 
     # editor
     VIM_COMMAND='/Applications/MacVim.app/Contents/MacOS/Vim'
 
-    export PATH=${PATH}:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/
+    vscode=/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/
+    export PATH="${vscode}":"${PATH}"
 
     # my utilities
-    export PATH=~/bin:${PATH}
+    export PATH=~/bin:"${PATH}"
+
 fi
 
 if [ -x "${VIM_COMMAND}" ]; then
